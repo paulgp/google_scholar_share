a32e962f5efa29e130169c0ed375d65719522c02
Paul Goldsmith-Pinkham <paulgp@gmail.com>
2022-01-11 09:45:43 -0500

    Update dictionary calls

--- pull_data.py
+++ pull_data.py
@@ -5,7 +5,7 @@ import csv
 # Fill in your serpapi api key
 # Fill in desired author_id from google scholar
 API_KEY = ""
-AUTHOR_ID = ""
+AUTHOR_ID = "b4cFNacAAAAJ"
 params = {
     "api_key": API_KEY, 
     "engine": "google_scholar_author",
@@ -18,13 +18,13 @@ results = search.get_dict()
 
 citation_data = {}
 for article in results['articles']:
-  article_title = article['title']
-  article_link = article['link']
-  article_authors = article['authors']
-  article_publication = article['publication']
-  cited_by = article['cited_by']['value']
-  cited_by_link = article['cited_by']['link']
-  article_year = article['year']
+  article_title = article.get('title')
+  article_link = article.get('link')
+  article_authors = article.get('authors')
+  article_publication = article.get('publication')
+  cited_by = article.get('cited_by').get('value')
+  cited_by_link = article.get('cited_by').get('link')
+  article_year = article.get('year')
 
   print(f"Title: {article_title}\nLink: {article_link}\nAuthors: {article_authors}\nPublication: {article_publication}\nCited by: {cited_by}\nCited by link: {cited_by_link}\nPublication year: {article_year}\n")
 
@@ -32,7 +32,7 @@ for article in results['articles']:
       "api_key" : API_KEY,
       "engine" : "google_scholar_author",
       "view_op" : "view_citation",
-      "citation_id": article['citation_id'],
+      "citation_id": article.get('citation_id'),
       "hl": "en"
   }
   search2 = GoogleSearch(params_cite)
@@ -40,24 +40,26 @@ for article in results['articles']:
   citation_data[article_title] = citation
 
 
-timeseries_citations = results['cited_by']['graph']
+timeseries_citations = results.get('cited_by').get('graph')
 cite_data = []
-cite_data = [[line["year"] for line in timeseries_citations], [line["citations"] for line in timeseries_citations], ["total" for line in timeseries_citations]]
+cite_data = [[line.get("year") for line in timeseries_citations],
+             [line.get("citations") for line in timeseries_citations],
+             ["total" for line in timeseries_citations]]
 
 for article in results['articles']:
-    try:
-        article_citations = citation_data[article['title']]['citation']['total_citations']['table']
-        cite_data[0].extend([line["year"] for line in article_citations])
-        cite_data[1].extend([line["citations"] for line in article_citations])
-        cite_data[2].extend([article['title'] for line in article_citations])
-    except KeyError:
-        print("No Data", article)
+  if citation_data.get(article.get('title')).get('error') is None:
+    article_citations = citation_data.get(article.get('title')).get('citation').get('total_citations').get('table')
+    cite_data[0].extend([line.get("year") for line in article_citations])
+    cite_data[1].extend([line.get("citations") for line in article_citations])
+    cite_data[2].extend([article.get('title') for line in article_citations])
+  else:
+    print("No cite data for %s" % article.get('title'))
 
 
 write_data = []
 for i in range(len(cite_data[0])):
     write_data.append([cite_data[0][i], cite_data[1][i], cite_data[2][i]])
 
-with open("time_series.csv", 'w') as f:
+with open("milgrom_time_series.csv", 'w') as f:
     writer = csv.writer(f)
     writer.writerows(write_data)
